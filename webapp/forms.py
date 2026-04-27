0eb792036724a5bdfb05cfa2ab6b6aea085da9f6
Aditya Mahajan <150649543+aadityamahajn@users.noreply.github.com>
2026-04-27 15:48:49 +0530

    Add files via upload

---
+++ webapp/forms.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms 
+from .models import Record
+from django.forms.widgets import PasswordInput, TextInput
+
+# - Register/Create a user
+class CreateUserForm(UserCreationForm):
+    
+    class Meta:
+        
+        model = User
+        fields = ["username", "password1", "password2"]
+        
+# - Login a user
+class LoginForm(AuthenticationForm):
+    
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+    
+# - Add a record
+class CreateRecordForm(forms.ModelForm):
+    
+    class Meta:
+        
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
+        
+# - Update a record
+class UpdateRecordForm(forms.ModelForm):
+    
+    class Meta:
+        
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']       
